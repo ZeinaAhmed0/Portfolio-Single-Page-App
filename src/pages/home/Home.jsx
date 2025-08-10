e5af095b05232987aedfa48887d8851f51f5d4b6
ZeinaAhmed0 <zeinaahmed.421z@gmail.com>
2025-08-09 17:28:52 -0800

    add btn

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import img from '/src/assets/profile-pic.png'
 import SocialLinks from '../../components/common/SocialLinks'
+import { Link } from 'react-router'
 
 export default function Home() {
   return (
@@ -9,6 +10,9 @@ export default function Home() {
         <div className='flex justify-center flex-col gap-5'>
           <h1 className='lg:text-6xl md:4xl sm:text-3xl text-xl font-bold w-fit'> Hi,I'am <br /> <span className='text-blue-600'>Marlon</span> <br /> Web Designer </h1>
           <SocialLinks />
+          <div className="contact-btn">
+            <Link to={'contact'} className='sm:w-2xs  p-1 bg-blue-600 text-white text-xl cursor-pointer px-6 py-2 rounded-xl :hover:bg-blue-300 duration-300'>contact us</Link>
+          </div>
         </div>
         <div className='overflow-hidden relative flex items-center justify-center'>
           <img src={img} className='object-cover fill pt-5 border rounded-[50%] border-blue-600 bg-blue-600' alt="transparent profile pic" />
