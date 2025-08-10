e237fe279764eac764436bc3d6e2ac766aee082c
ZeinaAhmed0 <zeinaahmed.421z@gmail.com>
2025-08-09 18:20:06 -0800

    handle images

--- src/components/common/WorkCard.jsx
+++ src/components/common/WorkCard.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import work1 from '/src/assets/work1.jpg';
+import work2 from '/src/assets/work2.jpg';
+import work3 from '/src/assets/work3.jpg';
+import work4 from '/src/assets/work4.jpg';
+import work5 from '/src/assets/work5.jpg';
+import work6 from '/src/assets/work6.jpg';
 
 export default function () {
-    let imgs = ["src/assets/work1.jpg", "src/assets/work2.jpg", "src/assets/work3.jpg", "src/assets/work4.jpg", "src/assets/work5.jpg", "src/assets/work6.jpg"];
+    const images = [work1, work2, work3, work4, work5, work6];
     return (
         <>
             <div className="lg:w-7xl md:w-3xl sm:w-xl w-xs grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 items-center justify-center gap-5 m-auto my-10 p-5">
                 {
-                    imgs.map((img, index) => {
+                    images.map((img, index) => {
                         return (
                             <div className="img-box overflow-hidden relative rounded-2xl" key={index}>
                                 <img className='fill object-cover hover:scale-110 transition duration-500' src={img} alt="work img" />
@@ -14,7 +20,6 @@ export default function () {
                         )
                     })
                 }
-
             </div>
 
         </>
